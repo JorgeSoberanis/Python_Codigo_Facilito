ac569f02baf7e203e8df2967249fb9c83f29a8f9
Jorge Soberanis <jsoberanisbalam@gmail.com>
2021-11-17 18:14:22 -0600

    Valores falsos

--- Ciclos y condiciones/none.py
+++ Ciclos y condiciones/none.py
@@ -1,7 +1,21 @@
 
-resultado = None
+resultado = None #False
 
 resultado=[0,1,2,3,0]
 
 print(resultado)
 print(type(resultado))
+
+#True (1) / False (0)
+
+"""
+False Todos estos valores son considerados falsos
+None
+0
+0.0
+"" / ""
+[]
+()
+{}
+"""
+
