acd3d29bbf02359251af2893776cbd2e9438728a
JorgeSoberanis <jsoberanisbalam@gmail.com>
2021-12-02 22:20:32 -0600

    PYTHON

---
+++ Ciclos y condiciones/condicinoales.py
@@ -0,0 +1,7 @@
+resultado = 15
+
+
+if resultado > 10 and resultado <=20:
+   print("La varaibles cumple con la condicion. " )
+else:
+    print("La variables no cumplen con las condiciones. ")
---
+++ Ciclos y condiciones/multiples.py
@@ -0,0 +1,16 @@
+calificaciones = 7
+
+if calificaciones == 10:
+    print("Felicidades,aprobaste con excelencia")
+
+elif calificaciones == 9 or calificaciones == 8:
+    print("Felicidades, aproabste bien")
+
+elif calificaciones == 7 or calificaciones == 6:
+    print("Aprobaste")
+
+else:
+    print("Reprobaste la materia.")
+
+
+
