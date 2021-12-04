9169cba5785743efdd6e50fb2f4d47d5f790971e
JorgeSoberanis <jsoberanisbalam@gmail.com>
2021-12-03 20:14:34 -0600

    Ciclo While

---
+++ Ciclos y condiciones/while.py
@@ -0,0 +1,19 @@
+"""contador = 1
+
+while contador <= 10:
+    print(contador)
+
+    contador  = contador +1
+#Cuenta solo hasta 10 y lo imprime incrementadno uno
+"""
+numero = 8
+contador_digitos = 0
+
+while numero >= 1:
+    contador_digitos = contador_digitos + 1
+
+    numero = numero / 10
+
+    print (contador_digitos)
+
+
