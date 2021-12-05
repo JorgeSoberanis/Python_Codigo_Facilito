81e3fda44c727f627219b88a14829f9a60625062
JorgeSoberanis <jsoberanisbalam@gmail.com>
2021-12-04 19:53:49 -0600

    FINALLY

--- Ciclos y condiciones/range.py
+++ Ciclos y condiciones/range.py
@@ -1,12 +1,18 @@
-"""""
+
 rango = range(21) #Listado de un rango del 0-20
 
 for valor in rango:
     #print(type(valor))
     print(valor)
 
-"""
+
 Rango = range(0 , 20 , 2)
 #stard, end, skips
 for c in Rango:
     print(c)
+
+
+numeros =[10,20,30,40,50]
+
+for indicem, numero in enumerate(numeros):
+    print(indicem,numero)
