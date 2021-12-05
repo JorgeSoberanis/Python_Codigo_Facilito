6b6ea1b55b52f48d8f0706038c57ccdca6493c5a
JorgeSoberanis <jsoberanisbalam@gmail.com>
2021-12-04 18:57:46 -0600

    range skips

--- Ciclos y condiciones/range.py
+++ Ciclos y condiciones/range.py
@@ -1,4 +1,12 @@
+"""""
 rango = range(21) #Listado de un rango del 0-20
 
 for valor in rango:
+    #print(type(valor))
     print(valor)
+
+"""
+Rango = range(0 , 20 , 2)
+#stard, end, skips
+for c in Rango:
+    print(c)
