6dfb8f815807fedef59cfa106fa5f27c635b30b0
JorgeSoberanis <jsoberanisbalam@gmail.com>
2021-12-04 18:25:50 -0600

    Fin del ciclo

---
+++ Ciclos y condiciones/for.py
@@ -0,0 +1,6 @@
+usuarios = ["Usuario1", "Usuario2", "Usuario3", "Usuario4"]
+
+for usuario in usuarios:
+    print(usuario)
+    
+
--- Ciclos y condiciones/while.py
+++ Ciclos y condiciones/while.py
@@ -6,14 +6,17 @@ while contador <= 10:
     contador  = contador +1
 #Cuenta solo hasta 10 y lo imprime incrementadno uno
 """
-numero = 8
+numero = 1
 contador_digitos = 0
-#
+
 while numero >= 1:
     #contador_digitos = contador_digitos + 1
     contador_digitos += 1
+
     numero = numero / 10
 
     print (contador_digitos)
-
+    
+else:
+    print(contador_digitos)
 
