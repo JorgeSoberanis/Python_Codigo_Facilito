9c0406c8233502f51dfe3860b0030398b247659a
JorgeSoberanis <jsoberanisbalam@gmail.com>
2021-12-12 01:17:12 -0600

    def suma

---
+++ Funciones/suma.py
@@ -0,0 +1,8 @@
+def suma():
+    numero_uno = int(input("Ingresa el primer numero entero: "))
+    numero_dos = int(input("Ingresa el segundo numero entero: "))
+    
+    resultado = numero_dos + numero_uno
+    print("La suma es:" , (resultado))
+    
+suma() 
