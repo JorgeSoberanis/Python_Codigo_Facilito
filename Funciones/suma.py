eab77d4bbe9abbe726de8ea51334be4d5b262af4
JorgeSoberanis <jsoberanisbalam@gmail.com>
2021-12-23 16:52:19 -0600

    Function Return

--- Funciones/suma.py
+++ Funciones/suma.py
@@ -1,4 +1,4 @@
-def suma():
+"""def suma():
     numero_uno = int(input("Ingresa el primer numero entero: "))
     numero_dos = int(input("Ingresa el segundo numero entero: "))
     
@@ -6,3 +6,13 @@ def suma():
     print("La suma es:" , (resultado))
     
 suma() 
+"""
+
+def suma (n1,n2):
+    return n1, n2, "La funcion retorna dos valores"
+
+numero_uno = int(input("Ingresa el primero numero entero: "))
+numero_dos = int(input("Ingresa el segundon numero entero:  "))
+
+resultado = suma(numero_dos,numero_uno)
+print(resultado)
