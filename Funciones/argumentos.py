558f0572699e53d792195a588b0b93b42e141ba7
JorgeSoberanis <jsoberanisbalam@gmail.com>
2022-01-02 22:05:49 -0600

    cambio a args por convencion

--- Funciones/argumentos.py
+++ Funciones/argumentos.py
@@ -1,10 +1,9 @@
-def promedio (*listado): #Toma todos los argumentos y crea una Tupla 
-    print(listado)
-    print(type(listado))
-    return sum(listado) / len(listado)
+def promedio (*args): #Toma todos los argumentos y crea una Tupla 
+    return sum(args) / len(args)
 
 resultado = promedio (10,10,5,7,10)
 print(resultado)
+
 
 
 
