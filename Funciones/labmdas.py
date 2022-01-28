6d32ed8979aaa3037dff8ab1cb29057cc5788027
JorgeSoberanis <jsoberanisbalam@gmail.com>
2022-01-27 18:21:46 -0600

    Function

--- Funciones/labmdas.py
+++ Funciones/labmdas.py
@@ -2,7 +2,12 @@ def centigrados_a_farenhite (grados):   #Definicion de la variable
     return grados * 1.8 + 32            #Formula de C° a F°
 
 
-mi_funcion = centigrados_a_farenhite
+mi_funcion = centigrados_a_farenhite   
 
 print(type(mi_funcion))
-print(mi_funcion(10))
+
+print (mi_funcion(10))
+
+                                        
+
+
